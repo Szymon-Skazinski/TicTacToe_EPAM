3d21892224373ea658172b63201f6428897650fa
Szymon Skaziński <63155273+Szymon-Skazinski@users.noreply.github.com>
2020-12-14 10:54:19 +0100

    Add checkState method to Arbiter class

--- src/main/java/com/epam/Arbiter.java
+++ src/main/java/com/epam/Arbiter.java
@@ -1,4 +1,17 @@
 package com.epam;
 
+import java.util.Arrays;
+
 public class Arbiter {
+
+
+    private static State checkState(Cell[] cells) {
+
+
+        if (Arrays.stream(cells).anyMatch(cell -> cell.getPlayer() == Player.NONE)) return State.PLAYING;
+        if (Arrays.stream(cells).allMatch(cell -> cell.getPlayer() == Player.X)) return State.X_WON;
+        else if (Arrays.stream(cells).allMatch(cell -> cell.getPlayer() == Player.Y)) return State.Y_WON;
+        return State.PLAYING;
+    }
+
 }
