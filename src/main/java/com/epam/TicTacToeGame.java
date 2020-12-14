83996e53c26a32f44271fe0e2a64d29e8470fd31
Szymon Skaziński <63155273+Szymon-Skazinski@users.noreply.github.com>
2020-12-14 23:57:26 +0100

    Add createBoard method to TicTacToeGame class

--- src/main/java/com/epam/TicTacToeGame.java
+++ src/main/java/com/epam/TicTacToeGame.java
@@ -1,4 +1,16 @@
 package com.epam;
 
 public class TicTacToeGame {
+
+    private Board createBoard(int size) {
+
+        Cell[][] cells = new Cell[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                cells[i][j] = new Cell(Player.NONE);
+            }
+        }
+        return new Board(cells);
+    }
 }
