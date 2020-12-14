52f7210888b4230ff36392cb77e3db5a3025aa72
Szymon Skaziński <63155273+Szymon-Skazinski@users.noreply.github.com>
2020-12-14 10:28:14 +0100

    Implement Cell class

--- src/main/java/com/epam/Cell.java
+++ src/main/java/com/epam/Cell.java
@@ -1,4 +1,18 @@
 package com.epam;
 
 public class Cell {
+
+    private Player player;
+
+    public Cell(Player player) {
+        this.player = player;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
 }
