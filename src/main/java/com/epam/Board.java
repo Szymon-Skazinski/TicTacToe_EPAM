007036e0cf46980531b3f54452d24782c16b519a
Szymon Skaziński <63155273+Szymon-Skazinski@users.noreply.github.com>
2020-12-14 10:28:47 +0100

    Implement Board class

--- src/main/java/com/epam/Board.java
+++ src/main/java/com/epam/Board.java
@@ -1,4 +1,18 @@
 package com.epam;
 
 public class Board {
+    private final Cell[][] board;
+
+    public Board() {
+        this.board = new Cell[][]{
+                {new Cell(Player.NONE), new Cell(Player.NONE), new Cell(Player.NONE)},
+                {new Cell(Player.NONE), new Cell(Player.NONE), new Cell(Player.NONE)},
+                {new Cell(Player.NONE), new Cell(Player.NONE), new Cell(Player.NONE)}
+        };
+    }
+
+    public Cell[][] getBoard() {
+        return board;
+    }
+
 }
